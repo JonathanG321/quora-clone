0bf5ce8854f904e35663d2152cabebe19c9e75e1
JonathanG321 <JonathanG321@ymail.com>
2020-08-17 16:41:50 -0400

    add express server in app.js

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const logger = require('morgan');
+const methodOverride = require('method-override');
 
 const app = express();
 
 app.use(logger('dev'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      const method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  }),
+);
 
 // app.use('/api', apiRouter);
 
