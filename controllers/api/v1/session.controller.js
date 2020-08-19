752307b09476600fdd574b094213db02cca4c736
JonathanG321 <JonathanG321@ymail.com>
2020-08-19 10:31:01 -0400

    add session create and derstroy

--- controllers/api/v1/session.controller.js
+++ controllers/api/v1/session.controller.js
@@ -2,6 +2,26 @@ const User = require('../../../models/user.model');
 const bcrypt = require('bcrypt');
 
 module.exports = {
-  async create(request, response, next) {},
-  destroy(request, response, next) {},
+  async create(request, response, next) {
+    try {
+      const { email: _email = 'INVALID', password } = request.body;
+      const email = _email.toLowerCase();
+      const user = await User.findOne({
+        where: { email },
+        attributes: { include: ['passwordDigest'] },
+      });
+      request.session.userId = user.id;
+      response.json({ id: user.id });
+    } catch (error) {
+      next(error);
+    }
+  },
+  destroy(request, response, next) {
+    try {
+      delete request.session.userId;
+      response.json({ status: 200, ok: true });
+    } catch (e) {
+      next(e);
+    }
+  },
 };
