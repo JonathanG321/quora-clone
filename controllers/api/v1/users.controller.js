6a688a1990c30d28cc0b91d2236287e88cb5e3cf
JonathanG321 <JonathanG321@ymail.com>
2020-08-19 12:00:48 -0400

    add create and get current user methods to users controller

--- controllers/api/v1/users.controller.js
+++ controllers/api/v1/users.controller.js
@@ -2,6 +2,28 @@ const Sequelize = require('sequelize');
 const User = require('../../../models/user.model');
 
 module.exports = {
-  async create(request, response, next) {},
-  async currentUser(request, response, next) {},
+  async create(request, response, next) {
+    try {
+      const { firstName, lastName, password = '', passwordConfirmation, email = '' } = request.body;
+      const newUser = {
+        firstName,
+        lastName,
+        password,
+        passwordConfirmation,
+        email: email.toLowerCase(),
+      };
+      const user = await User.create(newUser);
+      request.session.userId = user.id;
+      response.json({ id: user.id });
+    } catch (e) {
+      next(e);
+    }
+  },
+  async currentUser(request, response, next) {
+    try {
+      response.json(response.locals.currentUser);
+    } catch (e) {
+      next(e);
+    }
+  },
 };
