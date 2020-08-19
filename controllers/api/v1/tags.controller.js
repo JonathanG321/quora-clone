12a8998637683532ae64d6b63b13664c3d70ce7f
JonathanG321 <JonathanG321@ymail.com>
2020-08-19 16:04:52 -0400

    add tags controller with index, show, and create methods

---
+++ controllers/api/v1/tags.controller.js
@@ -0,0 +1,44 @@
+const Sequelize = require('sequelize');
+const User = require('../../../models/user.model');
+const Question = require('../../../models/question.model');
+const Tag = require('../../../models/tag.model');
+
+const TagsController = {
+  async index(request, response, next) {
+    try {
+      const tags = await Tag.findAll();
+      response.json(tags);
+    } catch (e) {
+      next(e);
+    }
+  },
+  async show(request, response, next) {
+    try {
+      const { id } = request.params;
+      const tag = await Tag.findOne({
+        where: { id },
+        include: [
+          {
+            model: Question,
+            include: { model: User },
+          },
+        ],
+      });
+      response.json(tag);
+    } catch (e) {
+      next(e);
+    }
+  },
+  async create(request, response, next) {
+    try {
+      const { name } = request.body;
+      const newTag = { name };
+      const tag = await Tag.create(newTag);
+      response.status(201).json(tag);
+    } catch (e) {
+      next(e);
+    }
+  },
+};
+
+module.exports = TagsController;
