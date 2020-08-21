70e3d6491171f6e6fb614ac2c40410a7ee48b1fe
JonathanG321 <JonathanG321@ymail.com>
2020-08-21 19:48:49 -0400

    mount spaces, topics and tags routers

--- routers/api/v1.router.js
+++ routers/api/v1.router.js
@@ -1,11 +1,20 @@
 const Router = require('express').Router;
 const questionsRouter = require('./v1/questions.router');
+const spacesRouter = require('./v1/spaces.router');
+const topicsRouter = require('./v1/topics.router');
+const tagsRouter = require('./v1/tags.router');
 const sessionRouter = require('./v1/session.router');
 const usersRouter = require('./v1/users.router');
 
 const router = new Router();
 
 router.use('/questions', questionsRouter);
+
+router.use('/spaces', spacesRouter);
+
+router.use('/topics', topicsRouter);
+
+router.use('/tags', tagsRouter);
 
 router.use('/session', sessionRouter);
 
