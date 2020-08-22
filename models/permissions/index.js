0bfdd344a3cf48c7371806ca896a1b2a37d7529a
JonathanG321 <JonathanG321@ymail.com>
2020-08-22 14:58:51 -0400

    add permissions for answers questions and replies

---
+++ models/permissions/index.js
@@ -0,0 +1,5 @@
+const QuestionPermissions = require('./questions');
+const AnswerPermissions = require('./answers');
+const ReplyPermissions = require('./replies');
+
+module.exports = { QuestionPermissions, AnswerPermissions, ReplyPermissions };
---
+++ models/permissions/questions.js
@@ -0,0 +1,39 @@
+function can(user, action, question) {
+  if (!user) {
+    return false;
+  }
+  switch (action) {
+    case 'edit':
+      return canEdit(user, question);
+    case 'delete':
+      return canDelete(user, question);
+    case 'like':
+      return canDislike(user, question);
+    case 'manage':
+      return canManage(user, question);
+    default:
+      return false;
+  }
+}
+
+function canEdit(user, question) {
+  return user.id === question.user.id;
+}
+
+function canDelete(user, question) {
+  return user.id === question.user.id;
+}
+
+function canDislike(user, question) {
+  return user.id !== question.user.id;
+}
+
+function cannot(user, action, question) {
+  return !can(user, action, question);
+}
+
+function canManage(user, question) {
+  return canEdit(user, question) && canDelete(user, question);
+}
+
+module.exports = { can, cannot };
