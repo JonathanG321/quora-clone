c673b747b83f0c1ec7a85a89e4423ba709393c69
JonathanG321 <JonathanG321@ymail.com>
2020-08-22 14:43:51 -0400

    add api Controller methods to api router

--- routers/api.router.js
+++ routers/api.router.js
@@ -1,8 +1,22 @@
 const Router = require('express').Router;
 const v1Router = require('./api/v1.router');
+const { RecordNotFoundError, ApiController } = require('../controllers/api.controller');
+const Sequelize = require('sequelize');
 
 const router = new Router();
 
 router.use('/v1', v1Router);
+
+router.use((err, req, res, next) => {
+  if (err instanceof RecordNotFoundError) {
+    return ApiController.recordNotFound(err, req, res);
+  }
+  if (err instanceof Sequelize.ValidationError) {
+    return ApiController.validationError(err, req, res);
+  }
+  return ApiController.serverError(err, req, res);
+});
+
+router.use('/*', ApiController.notFound);
 
 module.exports = router;
