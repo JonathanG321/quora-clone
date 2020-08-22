ffab892aadc66db5ed52c87f5341fdbec9a06d09
JonathanG321 <JonathanG321@ymail.com>
2020-08-21 20:14:51 -0400

    bugfix: votes router params now work as intended

--- routers/api/v1/votes.router.js
+++ routers/api/v1/votes.router.js
@@ -1,14 +1,14 @@
 const Router = require('express').Router;
 const ApiV1VotesController = require('../../../controllers/api/v1/votes.controller');
 
-const router = new Router();
+const router = new Router({ mergeParams: true });
 
 // /api/v1/questions/:questionId/answers/:answerId/votes
 
 router.post('/', ApiV1VotesController.create);
 
-router.patch('/:id', ApiV1VotesController.update);
+router.patch('/', ApiV1VotesController.update);
 
-router.delete('/:id', ApiV1VotesController.destroy);
+router.delete('/', ApiV1VotesController.destroy);
 
 module.exports = router;
