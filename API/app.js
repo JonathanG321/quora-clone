da664f14a1835f860c56af5f9b34d7e133d5043d
JonathanG321 <JonathanG321@ymail.com>
2020-08-26 10:23:55 -0400

    add cors

--- API/app.js
+++ API/app.js
@@ -3,14 +3,13 @@ const logger = require('morgan');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const redis = require('redis');
+const cors = require('cors');
 const RedisStore = require('connect-redis')(session);
 require('dotenv').config();
 require('./db/client');
 
 const Authentication = require('./middleware/authentication.middleware');
 const apiRouter = require('./routers/api.router');
-
-const redisClient = redis.createClient();
 
 const app = express();
 
@@ -19,6 +18,8 @@ app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const redisClientUrl = process.env.REDIS_URL || undefined;
+const redisClient = redis.createClient(redisClientUrl);
 redisClient.on('error', console.error);
 
 app.use(
@@ -42,6 +43,14 @@ app.use(
 );
 
 app.use(Authentication.setCurrentUser);
+
+const allowList = ['http://localhost:3001'];
+const corsOptions = {
+  origin: allowList,
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use('/api', apiRouter);
 
