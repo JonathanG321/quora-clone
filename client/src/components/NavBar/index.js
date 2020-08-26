9d0e5be8481f34feb5558e1db2442ebb15648c78
JonathanG321 <JonathanG321@ymail.com>
2020-08-26 14:56:34 -0400

    style navbar

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import { withRouter } from 'react-router-dom';
-import { withRouterPropTypes } from '../../PropTypes/withRouterPropTypes';
+// import { withRouterPropTypes } from '../../PropTypes/withRouterPropTypes';
 import { Session } from '../../requests/session';
-
 import './styles.scss';
+import Fa from '../common/Fa';
 
 function NavBar(props) {
   const { user, onSignOut } = props;
@@ -18,13 +18,14 @@ function NavBar(props) {
     });
   }
   return (
-    <nav className="nav navbar navbar-light bg-light left nav-pills">
+    <nav className="nav navbar navbar-light bg-light flex align-items-center">
       <div className="navbar-brand">
         <strong>Quora</strong>
       </div>
       <div className="nav-item">
         <NavLink className="nav-link" exact to="/">
-          <strong>Home</strong>
+          <Fa kind={'home'} color="#7b7b7d" />
+          <strong className="ml-2">Home</strong>
         </NavLink>
       </div>
       {!isSignedIn && (
