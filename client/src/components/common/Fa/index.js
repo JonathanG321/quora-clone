b9cd574bfac8cc2b0b3f1187438f10ab9672bdcd
JonathanG321 <JonathanG321@ymail.com>
2020-09-01 14:19:30 -0400

    add type prop to Fa

--- client/src/components/common/Fa/index.js
+++ client/src/components/common/Fa/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import classnames from 'classnames';
 
 function Fa(props) {
-  const { kind, spin = false, pulse = false, size = false, color = 'black' } = props;
+  const { kind, spin = false, pulse = false, size = false, color = 'black', type = 's' } = props;
   return (
     <i
       style={{ color: color }}
-      className={classnames(`fa fa-${kind}`, {
+      className={classnames(`fa${type} fa-${kind}`, {
         'fa-spin': spin,
         [`fa-${size}`]: size,
         'fa-pulse': pulse,
