a2f9da61f0be6732b48498c27543f0c2a1a090a8
JonathanG321 <JonathanG321@ymail.com>
2020-09-01 14:22:45 -0400

    add related questions section to questions show page

--- client/src/components/pages/QuestionsShowPage/index.js
+++ client/src/components/pages/QuestionsShowPage/index.js
@@ -6,7 +6,7 @@ import QuestionDetails from './QuestionDetails';
 import AnswerList from './AnswerList';
 import { Question } from '../../../requests/question';
 import Loading from '../../common/Loading';
-import './index.css';
+import './index.scss';
 
 class QuestionsShowPage extends Component {
   constructor(props) {
@@ -31,10 +31,15 @@ class QuestionsShowPage extends Component {
       return <h1>Question doesn't exist!</h1>;
     }
     return (
-      <main className="QuestionsShowPage">
-        <QuestionDetails {...question} />
-        <h2 style={{ marginTop: '3em' }}>Answers</h2>
-        <AnswerList answers={question.answers} />
+      <main className="QuestionsShowPage ml-5 mr-5 row">
+        <div className="col-7 mt-4">
+          <QuestionDetails {...question} />
+          <AnswerList answers={question.answers} />
+        </div>
+        <div className="col-5 mt-4">
+          <h5 className="mb-0">Related Questions</h5>
+          <hr />
+        </div>
       </main>
     );
   }
