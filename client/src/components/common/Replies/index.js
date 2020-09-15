56e234ced7d00f9b103b8f2e66774173e726ec8c
JonathanG321 <JonathanG321@ymail.com>
2020-09-15 16:41:20 -0400

    FE: refactor Replies component into class component

--- client/src/components/common/Replies/index.js
+++ client/src/components/common/Replies/index.js
@@ -1,35 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Reply from '../../../requests/reply';
 import PropTypes from 'prop-types';
 import ReplyForm from './ReplyForm';
 import { Link } from 'react-router-dom';
 import './styles.scss';
 import LoadMoreRepliesButton from './LoadMoreRepliesButton';
 
-function Replies(props) {
-  const { replies, onSubmitReplyForm, answerId, questionId, onClickLoadReplies } = props;
-  return (
-    <div>
-      <ReplyForm onSubmit={onSubmitReplyForm} answerId={answerId} questionId={questionId} />
-      {replies.map((reply, index) => {
-        return (
-          <div key={index} className="reply-card card-body pb-3">
-            {reply.user.firstName} {reply.user.lastName}
-            <span className="date">
-              {' '}
-              •{' '}
-              {new Date(reply.createdAt).toLocaleDateString(undefined, {
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric',
-              })}
-            </span>
-            <div>{reply.body}</div>
-          </div>
-        );
-      })}
-      <LoadMoreRepliesButton onClick={onClickLoadReplies} />
-    </div>
-  );
+class Replies extends Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount(props) {
+    const { answerId, questionId, limit, offset, onClickLoadReplies } = props;
+    onClickLoadReplies(answerId, questionId, limit, offset);
+  }
+  render(props) {
+    const { replies, onSubmitReplyForm, answerId, questionId, onClickLoadReplies } = props;
+    return (
+      <div>
+        <ReplyForm onSubmit={onSubmitReplyForm} answerId={answerId} questionId={questionId} />
+        {replies.map((reply, index) => {
+          return (
+            <div key={index} className="reply-card card-body pb-3">
+              {reply.user.firstName} {reply.user.lastName}
+              <span className="date">
+                {' '}
+                •{' '}
+                {new Date(reply.createdAt).toLocaleDateString(undefined, {
+                  year: 'numeric',
+                  month: 'long',
+                  day: 'numeric',
+                })}
+              </span>
+              <div>{reply.body}</div>
+            </div>
+          );
+        })}
+        <LoadMoreRepliesButton onClick={onClickLoadReplies} />
+      </div>
+    );
+  }
 }
 
 Replies.propTypes = {
