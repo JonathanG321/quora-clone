e737e89ec3f1184e26efc086083c417fd23fb1fe
JonathanG321 <JonathanG321@ymail.com>
2020-09-15 23:09:25 -0400

    FE: fix reply requests

--- client/src/requests/reply.js
+++ client/src/requests/reply.js
@@ -1,22 +1,19 @@
 import { API_URL, jsonHeaders } from './base';
 
 export const Reply = {
-  create(newReply, answerId, questionId) {
-    return fetch(`${API_URL}/questions/${questionId}/answers/${answerId}/replies`, {
+  create(newReply, answerId) {
+    return fetch(`${API_URL}/answers/${answerId}/replies`, {
       method: 'POST',
       headers: jsonHeaders,
       credentials: 'include',
       body: JSON.stringify(newReply),
     }).then((res) => res.json());
   },
-  get(answerId, questionId, limit, offset) {
-    return fetch(
-      `${API_URL}/questions/${questionId}/answers/${answerId}/replies?limit=${limit}&offset=${offset}`,
-      {
-        method: 'GET',
-        headers: jsonHeaders,
-        credentials: 'include',
-      },
-    ).then((res) => res.json());
+  get(answerId, limit, offset) {
+    return fetch(`${API_URL}/answers/${answerId}/replies?limit=${limit}&offset=${offset}`, {
+      method: 'GET',
+      headers: jsonHeaders,
+      credentials: 'include',
+    }).then((res) => res.json());
   },
 };
