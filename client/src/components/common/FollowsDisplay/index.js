8e7e8e323eb817823ee32469ef2d1c61e0e046de
JonathanG321 <JonathanG321@ymail.com>
2020-09-18 19:27:49 -0400

    turn follows display into class component

--- client/src/components/common/FollowsDisplay/index.js
+++ client/src/components/common/FollowsDisplay/index.js
@@ -1,27 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { Feed } from '../../../requests/feed';
+import Loading from '../Loading';
 
-function FollowsDisplay(props) {
-  const { follows } = props;
-  return (
-    <ul>
-      <li className="max-width">
-        <Link className="mt-1 mb-1 follow-list-item" to={`/`}>
-          Feed
-        </Link>
-      </li>
-      {follows.map((follow, id) => {
-        return (
-          <li key={id} className="max-width">
-            <Link className="mt-1 mb-1 follow-list-item" to={`/${follow.type}/${follow.id}`}>
-              {follow.name}
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
-  );
+class FollowsDisplay extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      follows: [],
+    };
+  }
+  async componentDidMount() {
+    const follows = await Feed.getFollows();
+    this.setState(follows);
+  }
+  render() {
+    const { follows } = this.state;
+    console.log(follows);
+    if (follows.length === 0) {
+      return <Loading />;
+    }
+    return (
+      <ul>
+        <li className="max-width">
+          <Link className="mt-1 mb-1 follow-list-item" to={`/`}>
+            Feed
+          </Link>
+        </li>
+        {follows.map((follow, id) => {
+          return (
+            <li key={id} className="max-width">
+              <Link className="mt-1 mb-1 follow-list-item" to={`/${follow.type}/${follow.id}`}>
+                {follow.name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
 }
 
 FollowsDisplay.propTypes = {
@@ -32,10 +50,6 @@ FollowsDisplay.propTypes = {
       type: PropTypes.oneOf(['spaces', 'topics']),
     }),
   ),
-};
-
-FollowsDisplay.defaultProps = {
-  follows: {},
 };
 
 export default FollowsDisplay;
